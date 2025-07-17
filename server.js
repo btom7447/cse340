3d7c524aa28a7f682e7214b29cd518cb3388cbf2
btom7447 <tombenjamin7447@gmail.com>
2025-07-17 14:35:46 +0100

    update to learning activity

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const expressLayouts = require('express-ejs-layouts');
-
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 // View Engine and Templates
 app.set("view engine", "ejs")
@@ -22,9 +23,14 @@ app.set("layout", "./layouts/layout")
  *************************/
 app.use(static)
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
+// app.get("/", function(req, res){
+//   res.render("index", {title: "Home"})
+// })
 
 /* ***********************
  * Local Server Information
